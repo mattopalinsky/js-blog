f1905bd81142c28ca78f74a721c8d2e1b5fd6de2
Mateusz Opaliński <mateuszopalinski96@gmail.com>
2022-02-20 16:17:48 +0100

    use 'this'

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
 
@@ -13,6 +14,8 @@ function titleClickHandler(event){
 }
 
   /* add class 'active' to the clicked link */
+
+  clickedElement.classList.add('active');
 
   /* remove class 'active' from all articles */
 
