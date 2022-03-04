efe60646b65069f2a2d1cc58ad280f50830ca793
Mateusz Opaliński <mateuszopalinski96@gmail.com>
2022-03-04 22:11:51 +0100

    add generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -41,4 +41,46 @@ const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
-}
+ }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector).innerHTML = '';
+
+  /* for each article */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+
+   for(let article of articles) {
+
+    /* get the article id */
+
+    const articleId = article.getAttribute('id');
+
+    /* find the title element and get the tittle form the tittle element */
+
+    const articleTitle = article.quetrySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>'+ articleTitle +'</span></a></li>';
+    console.log(linkHTML);
+
+    /* insert link into titleList */
+
+    html = html + linkHTML;
+    console.log(html);
+   }
+
+   titleList.innerHTML = html;
+  }
+
+generateTitleLinks();
