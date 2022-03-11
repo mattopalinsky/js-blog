250fdab45a9e342f9f71956becf7be8eb668acc1
Mateusz Opaliński <mateuszopalinski96@gmail.com>
2022-03-11 06:30:13 +0100

    add generate tags

--- js/script.js
+++ js/script.js
@@ -88,3 +88,62 @@ function generateTitleLinks(){
 
 generateTitleLinks();
 }
+
+const optArticleTagsSelector = '.post-tags .list';
+
+function generateTags(){
+
+  /*find all articles */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log(articles);
+
+  /*START LOOP: for every article: */
+
+  for (let article of articles) {
+
+    /* find tags wrapper */
+
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+    console.log(tagsWrapper);
+
+    /*make html variable with empty string */
+
+    let html = '';
+
+    /*get tags from data-tags attribute */
+
+    const articleTags = article.getAttribute('data-tags');
+    console.log(articleTags);
+
+    /*split tags into array */
+
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
+
+    /*START LOOP: for each tag */
+
+    for(let tag of articleTagsArray){
+      console.log(tag);
+
+      /*generate HTML of the link */
+
+      const tagsLinkHTML = '<li><a href="#tag-' + tag + '">'  + tag + '</a></li>';
+      console.log(tagsLinkHTML);
+
+      /*add generated code to html variable */
+
+      html = html + tagsLinkHTML;
+
+    /* END LOOP: for each tag */
+    }
+
+    /*insert HTML of all the links into the tags wrapper */
+
+    tagsWrapper.innerHTML = html;
+
+  /* END LOOP: for every article: */
+  }
+}
+
+generateTags();
